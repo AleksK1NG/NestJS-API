f698579d5236f34b542aae1132edd1e6c637f004
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-04 18:48:07 +0300

    feat: add db module and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { PostsModule } from './posts/posts.module';
+import { PostsModule } from './posts/posts.module'
+import { DatabaseModule } from './database/database.module'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from 'joi'
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    PostsModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
