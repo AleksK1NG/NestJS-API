ee997a63b1dec53ea1c765146caf8e76314b2881
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 17:46:32 +0300

    feat: add Categories Repository

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { CategoriesController } from './categories.controller'
 import { CategoriesService } from './categories.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { CategoriesRepository } from './categories.repository'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoriesRepository])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
 })
