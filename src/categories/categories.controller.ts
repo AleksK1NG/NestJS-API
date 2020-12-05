0047906e0a5cb92af260c785de0b5cb33419de89
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 17:54:23 +0300

    feat: add Categories Controller

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Patch, Post, UseGuards } from '@nestjs/common'
+import { CategoriesService } from './categories.service'
+import { Category } from './category.entity'
+import CreateCategoryDto from './dto/create-category.dto'
+import JwtAuthenticationGuard from '../auth/guards/jwt-authentication.guard'
+import UpdateCategoryDto from './dto/update-category.dto'
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Get()
+  getAllCategories(): Promise<Category[]> {
+    return this.categoriesService.getAllCategories()
+  }
+
+  @Get(':id')
+  getCategoryById(id: number): Promise<Category> {
+    return this.categoriesService.getCategoryById(id)
+  }
+
+  @Post()
+  @UseGuards(JwtAuthenticationGuard)
+  createCategory(@Body() createCategoryDto: CreateCategoryDto): Promise<Category> {
+    return this.categoriesService.createCategory(createCategoryDto)
+  }
+
+  @Patch(':id')
+  @UseGuards(JwtAuthenticationGuard)
+  async updateCategory(updateCategoryDto: UpdateCategoryDto): Promise<Category> {
+    return this.categoriesService.updateCategory(updateCategoryDto)
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthenticationGuard)
+  async deleteCategory(id: number): Promise<void> {
+    return this.categoriesService.deleteCategory(id)
+  }
+}
