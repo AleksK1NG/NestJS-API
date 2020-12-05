a10b04f8c32a0bc820bfb71f595e376df712f4ee
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 18:10:54 +0300

    feat: add posts handlers update

--- src/posts/post.repository.ts
+++ src/posts/post.repository.ts
@@ -3,40 +3,41 @@ import Post from './post.entity'
 import CreatePostDto from './dto/createPost.dto'
 import UpdatePostDto from './dto/updatePost.dto'
 import PostNotFoundException from './exceptions/postNotFound.exception'
+import User from '../users/entities/user.entity'
 
 @EntityRepository(Post)
 export class PostRepository extends Repository<Post> {
   async getAllPosts(): Promise<Post[]> {
-    return this.find()
+    return this.find({ relations: ['author'] })
   }
 
   async getPostById(id: number): Promise<Post> {
-    const post = await this.findOne({ id })
+    const post = await this.findOne(id, { relations: ['author'] })
     if (!post) throw new PostNotFoundException(id)
     return post
   }
 
-  async createPost(createPostDto: CreatePostDto): Promise<Post> {
+  async createPost(createPostDto: CreatePostDto, user: User): Promise<Post> {
     const { title, content } = createPostDto
     const post = this.create({
       title,
       content,
+      author: user,
     })
     return this.save(post)
   }
 
-  async updatePost(updatePostDto: UpdatePostDto): Promise<Post> {
-    const { id, ...rest } = updatePostDto
-    const updatedPost = await this.preload({
-      id,
-      ...rest,
-    })
-    if (!updatedPost) throw new PostNotFoundException(id)
-    return updatedPost
+  async updatePost(id: number, updatePostDto: UpdatePostDto): Promise<Post> {
+    await this.update(id, updatePostDto)
+    const updatedPost = await this.findOne(id, { relations: ['author'] })
+    if (updatedPost) {
+      return updatedPost
+    }
+    throw new PostNotFoundException(id)
   }
 
   async deletePost(id: number): Promise<void> {
     const result = await this.delete({ id })
-    if (result.affected === 0) throw new PostNotFoundException(id)
+    if (!result.affected) throw new PostNotFoundException(id)
   }
 }
