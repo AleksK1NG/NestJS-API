7713c2ecf049743fcc1a697934421d335d0b769b
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 19:07:45 +0300

    feat: add aws service methods

--- src/aws/aws.module.ts
+++ src/aws/aws.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common'
 import { AwsService } from './aws.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AwsRepository } from './aws.repository'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([AwsRepository]), ConfigModule],
   providers: [AwsService],
+  exports: [AwsService],
 })
 export class AwsModule {}
