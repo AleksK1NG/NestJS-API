d7171a39b549a0d3cb4323a9512a8bae7c246b7c
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 12:18:26 +0300

    feat: add user module

---
+++ src/users/users.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { UsersService } from './users.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UsersRepository } from './users.repository'
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UsersRepository])],
+  providers: [UsersService],
+})
+export class UsersModule {}
