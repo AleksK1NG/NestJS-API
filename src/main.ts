f536b84fa350e467741ef7f803166609b9235976
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 18:49:14 +0300

    feat: add aws sdk

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory, Reflector } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as cookieParser from 'cookie-parser'
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common'
+import { config } from 'aws-sdk'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
   app.use(cookieParser())
+  const configService = app.get(ConfigService)
+
+  config.update({
+    accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
+    secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
+    region: configService.get('AWS_REGION'),
+  })
   await app.listen(5000)
 }
 bootstrap()
