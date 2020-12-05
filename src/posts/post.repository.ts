571dc1a4fe6cd4e54d4be0f52363b7f53bdf3699
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 11:46:44 +0300

    feat: add post repository crud methods

--- src/posts/post.repository.ts
+++ src/posts/post.repository.ts
@@ -1,10 +1,39 @@
 import { EntityRepository, Repository } from 'typeorm'
 import Post from './post.entity'
+import CreatePostDto from './dto/createPost.dto'
+import UpdatePostDto from './dto/updatePost.dto'
+import { NotFoundException } from '@nestjs/common'
 
 @EntityRepository(Post)
 export class PostRepository extends Repository<Post> {
   async getAllPosts(): Promise<Post[]> {
-    const posts = await this.find()
-    return posts
+    return this.find()
+  }
+
+  async getPostById(id: number): Promise<Post> {
+    const post = await this.findOne({ id })
+    if (!post) throw new NotFoundException({ message: `Post with ID "${id}" not found` })
+    return post
+  }
+
+  async createPost(createPostDto: CreatePostDto): Promise<Post> {
+    const { title, content } = createPostDto
+    const post = this.create({
+      title,
+      content,
+    })
+    return this.save(post)
+  }
+
+  async updatePost(updatePostDto: UpdatePostDto) {
+    const { id, ...rest } = updatePostDto
+    const updatedPost = await this.update({ id }, { ...rest })
+    console.log('updatedPost ', updatedPost)
+    return updatedPost
+  }
+
+  async deletePost(id: number): Promise<void> {
+    const result = await this.delete({ id })
+    if (result.affected === 0) throw new NotFoundException({ message: `Post with ID "${id}" not found` })
   }
 }
