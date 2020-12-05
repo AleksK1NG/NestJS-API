ec3292dae42a5befac9d4d525ac1b9b4633049cb
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 12:43:19 +0300

    feat: add auth service register method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,30 @@
-import { Injectable } from '@nestjs/common'
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { UsersService } from '../users/users.service'
+import RegisterDto from './dto/register.dto'
+import * as bcrypt from 'bcrypt'
+import PostgresErrorCode from '../database/postgresErrorCode.enum'
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
+
+  async register(registerDto: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(registerDto.password, 10)
+    try {
+      const createdUser = await this.usersService.createUser({
+        ...registerDto,
+        password: hashedPassword,
+      })
+
+      createdUser.password = null
+      return createdUser
+    } catch (e) {
+      if (e?.code === PostgresErrorCode.UniqueViolation) {
+        throw new HttpException('User with that email already exists', HttpStatus.BAD_REQUEST)
+      }
+      throw new HttpException('Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+  }
+
+
 }
