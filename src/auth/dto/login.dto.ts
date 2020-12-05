15c5a76156a32f9dd6d9dcc162512cc9369634ea
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 15:25:50 +0300

    feat: add auth validations with class validator

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator'
+
 export class LogInDto {
+  @IsEmail()
   email: string
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
   password: string
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as cookieParser from 'cookie-parser'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
   app.use(cookieParser())
   await app.listen(5000)
 }
