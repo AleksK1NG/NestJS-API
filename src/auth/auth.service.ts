18769085ffc56888d08716d56119d8c8d4732a46
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 13:03:19 +0300

    feat: add auth service getAuthenticatedUser jwt and cookie methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,8 +23,8 @@ export class AuthService {
         ...registerDto,
         password: hashedPassword,
       })
-
       createdUser.password = undefined
+
       return createdUser
     } catch (e) {
       if (e?.code === PostgresErrorCode.UniqueViolation) {
@@ -34,9 +34,30 @@ export class AuthService {
     }
   }
 
-  getCookieWithJwtToken(userId: number) {
+  getCookieWithJwtToken(userId: number): string {
     const payload: TokenPayload = { userId }
     const token = this.jwtService.sign(payload)
     return `Authentication=${token}; HttpOnly; Path=/; Max-Age=${this.configService.get('JWT_EXPIRATION_TIME')}`
+  }
+
+  getCookieForLogOut(): string {
+    return `Authentication=; HttpOnly; Path=/; Max-Age=0`
+  }
+
+  async getAuthenticatedUser(email: string, plainTextPassword: string): Promise<User> {
+    try {
+      const user = await this.usersService.getByEmail(email)
+      await this.verifyPassword(plainTextPassword, user.password)
+      return user
+    } catch (e) {
+      throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST)
+    }
+  }
+
+  async verifyPassword(plainTextPassword: string, hashedPassword: string): Promise<void> {
+    const isValid = await bcrypt.compare(plainTextPassword, hashedPassword)
+    if (!isValid) {
+      throw new HttpException('Wrong credentials provided', HttpStatus.BAD_REQUEST)
+    }
   }
 }
