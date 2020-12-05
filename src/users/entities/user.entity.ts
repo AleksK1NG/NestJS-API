5390c677426295ed4cf84cb4a617824b12dae6b9
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 16:16:27 +0300

    feat: add address entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,32 @@
+import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Exclude } from 'class-transformer'
+import Address from './address.entity'
+import Post from '../../posts/post.entity'
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  public id?: number
+
+  @Column({ unique: true })
+  public email: string
+
+  @Column()
+  public name: string
+
+  @Column()
+  @Exclude()
+  public password: string
+
+  @OneToOne(() => Address, {
+    eager: true,
+    cascade: true,
+  })
+  @JoinColumn()
+  public address: Address
+
+  @OneToMany(() => Post, (post: Post) => post.author)
+  public posts: Post[]
+}
+
+export default User
