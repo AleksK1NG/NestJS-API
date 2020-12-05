89df673e75aacb434ca2ad8381bd6d7fa24cf674
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 19:03:30 +0300

    feat: add aws deletePublicFile method

--- src/aws/aws.repository.ts
+++ src/aws/aws.repository.ts
@@ -2,6 +2,7 @@ import { EntityRepository, Repository } from 'typeorm'
 import PublicFile from './public-file.entity'
 import { S3 } from 'aws-sdk'
 import { v4 as uuid } from 'uuid'
+import { NotFoundException } from '@nestjs/common'
 
 @EntityRepository(PublicFile)
 export class AwsRepository extends Repository<PublicFile> {
@@ -21,5 +22,20 @@ export class AwsRepository extends Repository<PublicFile> {
     })
 
     return this.save(newFile)
+  }
+
+  async deletePublicFile(fileId: number, bucket: string) {
+    const file = await this.findOne({ id: fileId })
+    if (!file) throw new NotFoundException(`File with ID: ${fileId} not found`)
+
+    const s3 = new S3()
+    await s3
+      .deleteObject({
+        Bucket: bucket,
+        Key: file.key,
+      })
+      .promise()
+
+    await this.delete(fileId)
   }
 }
