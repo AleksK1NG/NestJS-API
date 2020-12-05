14c1637d9ff402990628623e7f05d7f76003375d
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 12:20:38 +0300

    feat: add UsersService methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common'
+import { UsersRepository } from './users.repository'
+import CreateUserDto from './dto/create-user.dto'
+import User from './user.entity'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    return this.usersRepository.createUser(createUserDto)
+  }
+
+  async getByEmail(email: string): Promise<User> {
+    return this.usersRepository.getByEmail(email)
+  }
+
+  async getById(id: number): Promise<User> {
+    return this.usersRepository.getById(id)
+  }
+}
