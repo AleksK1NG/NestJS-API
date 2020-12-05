e8245bf0841dfdde0c2793f8ee9da7a5be68f2c2
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 12:34:50 +0300

    feat: add auth domain module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common'
+import { UsersService } from '../users/users.service'
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+}
