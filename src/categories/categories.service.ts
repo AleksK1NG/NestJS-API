b90b567e23d77659c6faaa33cb088e571610ad4c
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 17:49:16 +0300

    feat: add Categories Service

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,7 +1,30 @@
 import { Injectable } from '@nestjs/common'
 import { CategoriesRepository } from './categories.repository'
+import { Category } from './category.entity'
+import CreateCategoryDto from './dto/create-category.dto'
+import UpdateCategoryDto from './dto/update-category.dto'
 
 @Injectable()
 export class CategoriesService {
   constructor(private readonly categoriesRepository: CategoriesRepository) {}
+
+  async getAllCategories(): Promise<Category[]> {
+    return this.categoriesRepository.getAllCategories()
+  }
+
+  async getCategoryById(id: number): Promise<Category> {
+    return this.categoriesRepository.getCategoryById(id)
+  }
+
+  async createCategory(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    return this.categoriesRepository.createCategory(createCategoryDto)
+  }
+
+  async updateCategory(updateCategoryDto: UpdateCategoryDto): Promise<Category> {
+    return this.categoriesRepository.updateCategory(updateCategoryDto)
+  }
+
+  async deleteCategory(id: number): Promise<void> {
+    return this.categoriesRepository.deleteCategory(id)
+  }
 }
