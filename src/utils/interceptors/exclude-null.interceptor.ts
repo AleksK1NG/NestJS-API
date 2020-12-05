31ee5b67face62ebdd671c3ad526f20a0e9a57fe
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 15:43:43 +0300

    feat: add ExcludeNullInterceptor ad update tsconfig.json

---
+++ src/utils/interceptors/exclude-null.interceptor.ts
@@ -0,0 +1,11 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+import recursivelyStripNullValues from '../recursivelyStripNullValues'
+
+@Injectable()
+export class ExcludeNullInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(map((value) => recursivelyStripNullValues(value)))
+  }
+}
