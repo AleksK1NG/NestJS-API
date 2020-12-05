28bdf43fe59c3695c9ed564f825ad680add90437
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-05 15:25:50 +0300

    feat: add auth validations with class validator

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,6 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator'
+
 export class RegisterDto {
+  @IsEmail()
   email: string
+
+  @IsString()
+  @IsNotEmpty()
   name: string
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
   password: string
 }
 
