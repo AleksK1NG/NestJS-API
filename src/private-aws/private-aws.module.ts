dff99b213255a60f1a61f745075c6e559a4038b6
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-06 11:23:13 +0300

    feat: add private aws service methods

--- src/private-aws/private-aws.module.ts
+++ src/private-aws/private-aws.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
-import { PrivateAwsService } from './private-aws.service';
+import { Module } from '@nestjs/common'
+import { PrivateAwsService } from './private-aws.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { PrivateAwsRepository } from './private-aws.repository'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  providers: [PrivateAwsService]
+  imports: [TypeOrmModule.forFeature([PrivateAwsRepository]), ConfigModule],
+  providers: [PrivateAwsService],
+  exports: [PrivateAwsService],
 })
 export class PrivateAwsModule {}
