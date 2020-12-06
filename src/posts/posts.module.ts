cd1e6ee6f0bace5a397fffffd2ac94af12af2510
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-06 13:46:31 +0300

    feat: add Posts Search Service

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,10 +3,12 @@ import { PostsController } from './posts.controller'
 import { PostsService } from './posts.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { PostRepository } from './post.repository'
+import { SearchModule } from '../search/search.module'
+import { PostsSearchService } from './postsSearch.service'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PostRepository])],
+  imports: [TypeOrmModule.forFeature([PostRepository]), SearchModule],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService, PostsSearchService],
 })
 export class PostsModule {}
