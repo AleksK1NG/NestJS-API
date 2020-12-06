70e6a9275cf8c36e039db9f0ff7acae150510430
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-06 12:07:44 +0300

    feat: add PrivateAwsRepository config refactor

--- src/private-aws/private-aws.repository.ts
+++ src/private-aws/private-aws.repository.ts
@@ -3,20 +3,29 @@ import PrivateFile from './private-file.entity'
 import { S3 } from 'aws-sdk'
 import { v4 as uuid } from 'uuid'
 import { NotFoundException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 @EntityRepository(PrivateFile)
 export class PrivateAwsRepository extends Repository<PrivateFile> {
-  async uploadPrivateFile(
-    ownerId: number,
-    data: Buffer,
-    filename: string,
-    bucket: string,
-    options: S3.Types.ClientConfiguration,
-  ): Promise<PrivateFile> {
-    const s3 = new S3(options)
+  constructor(private readonly configService: ConfigService) {
+    super()
+  }
+  private getS3Instance() {
+    return new S3({
+      accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      endpoint: this.configService.get('AWS_ENDPOINT'),
+      s3ForcePathStyle: this.configService.get('AWS_S3_FORCE_PATH_STYLE'),
+      signatureVersion: this.configService.get('AWS_SIGNATURE_VERSION'),
+    })
+  }
+
+  async uploadPrivateFile(ownerId: number, data: Buffer, filename: string): Promise<PrivateFile> {
+    const s3 = this.getS3Instance()
+    const bucketName = this.configService.get('AWS_PUBLIC_BUCKET_NAME')
     const uploadedResult = await s3
       .upload({
-        Bucket: bucket,
+        Bucket: bucketName,
         Body: data,
         Key: `${uuid()}-${filename}`,
       })
@@ -32,18 +41,14 @@ export class PrivateAwsRepository extends Repository<PrivateFile> {
     return this.save(newFile)
   }
 
-  async getPrivateFile(
-    fileId: number,
-    bucket: string,
-    options: S3.Types.ClientConfiguration,
-  ): Promise<Record<string, any>> {
-    const s3 = new S3(options)
-
+  async getPrivateFile(fileId: number): Promise<Record<string, any>> {
+    const s3 = this.getS3Instance()
+    const bucketName = this.configService.get('AWS_PUBLIC_BUCKET_NAME')
     const fileInfo = await this.findOne({ id: fileId }, { relations: ['owner'] })
     if (fileInfo) {
       const stream = await s3
         .getObject({
-          Bucket: bucket,
+          Bucket: bucketName,
           Key: fileInfo.key,
         })
         .createReadStream()
@@ -55,10 +60,11 @@ export class PrivateAwsRepository extends Repository<PrivateFile> {
     throw new NotFoundException(`File with id ${fileId} not found`)
   }
 
-  async generatePreassignedUrl(key: string, bucket: string, options: S3.Types.ClientConfiguration): Promise<string> {
-    const s3 = new S3(options)
+  async generatePreassignedUrl(key: string): Promise<string> {
+    const s3 = this.getS3Instance()
+    const bucketName = this.configService.get('AWS_PUBLIC_BUCKET_NAME')
     return s3.getSignedUrlPromise('getObject', {
-      Bucket: bucket,
+      Bucket: bucketName,
       Key: key,
     })
   }
--- src/private-aws/private-aws.service.ts
+++ src/private-aws/private-aws.service.ts
@@ -1,36 +1,21 @@
 import { Injectable } from '@nestjs/common'
 import { PrivateAwsRepository } from './private-aws.repository'
-import { ConfigService } from '@nestjs/config'
 import PrivateFile from './private-file.entity'
 
 @Injectable()
 export class PrivateAwsService {
-  private readonly awsS3Options = {}
-  constructor(
-    private readonly privateAwsRepository: PrivateAwsRepository,
-    private readonly configService: ConfigService,
-  ) {
-    this.awsS3Options = {
-      accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY'),
-      endpoint: configService.get('AWS_ENDPOINT'),
-      s3ForcePathStyle: configService.get('AWS_S3_FORCE_PATH_STYLE'),
-      signatureVersion: configService.get('AWS_SIGNATURE_VERSION'),
-    }
-  }
+  constructor(private readonly privateAwsRepository: PrivateAwsRepository) {}
 
   async uploadPrivateFile(dataBuffer: Buffer, ownerId: number, filename: string): Promise<PrivateFile> {
-    const bucket = this.configService.get('AWS_PUBLIC_BUCKET_NAME')
-    return this.privateAwsRepository.uploadPrivateFile(ownerId, dataBuffer, filename, bucket, this.awsS3Options)
+    return this.privateAwsRepository.uploadPrivateFile(ownerId, dataBuffer, filename)
   }
 
   async getPrivateFile(fileId: number): Promise<Record<string, any>> {
-    const bucket = this.configService.get('AWS_PUBLIC_BUCKET_NAME')
-    return this.privateAwsRepository.getPrivateFile(fileId, bucket, this.awsS3Options)
+    console.log(111)
+    return this.privateAwsRepository.getPrivateFile(fileId)
   }
 
   async generatePreassignedUrl(key: string): Promise<string> {
-    const bucket = this.configService.get('AWS_PUBLIC_BUCKET_NAME')
-    return this.privateAwsRepository.generatePreassignedUrl(key, bucket, this.awsS3Options)
+    return this.privateAwsRepository.generatePreassignedUrl(key)
   }
 }
