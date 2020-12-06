a1ffcd4a241fa88ee92b0e2ebc9ac6717057082d
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-06 12:36:44 +0300

    feat: add UsersController private aws handlers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,21 @@
-import { Controller, Delete, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common'
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+  Res,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common'
 import { UsersService } from './users.service'
 import JwtAuthenticationGuard from '../auth/guards/jwt-authentication.guard'
 import { FileInterceptor } from '@nestjs/platform-express'
 import RequestWithUser from '../auth/interfaces/requestWithUser.interface'
+import { Response } from 'express'
 
 @Controller('api/v1/users')
 export class UsersController {
@@ -19,5 +32,25 @@ export class UsersController {
   @UseGuards(JwtAuthenticationGuard)
   async deleteAvatar(@Req() req: RequestWithUser) {
     return this.usersService.deleteAvatar(req.user.id)
+  }
+
+  @Get('files')
+  @UseGuards(JwtAuthenticationGuard)
+  async getAllPrivateFiles(@Req() req: RequestWithUser) {
+    return this.usersService.getAllPrivateFiles(req.user.id)
+  }
+
+  @Post('files')
+  @UseGuards(JwtAuthenticationGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  async addPrivateFile(@Req() req: RequestWithUser, @UploadedFile() file: Record<string, any>) {
+    return this.usersService.uploadPrivateFile(req.user.id, file.buffer, file.req.user.id)
+  }
+
+  @Get('files/:id')
+  @UseGuards(JwtAuthenticationGuard)
+  async getPrivateFile(@Req() req: RequestWithUser, @Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+    const file = await this.usersService.getPrivateFile(req.user.id, id)
+    file.stream.pipe(res)
   }
 }
